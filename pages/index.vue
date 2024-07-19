5c2011d5d838e03591cebc8b5af6b356ddbe7f34
Sooraj P R <19621059+soorajiam@users.noreply.github.com>
2024-07-19 12:31:54 +0100

    changing some ui, nput fields-> changing some to tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,70 @@
 <template>
   <div class="resume-builder">
-    <h1>Resume Builder</h1>
+    <h1 class="text-3xl">Resume Builder</h1>
     
-    <h2>Personal Information</h2>
-    <input v-model="resume.personalInfo.name" placeholder="Full Name" />
-    <input v-model="resume.personalInfo.phone" placeholder="Phone Number" />
-    <input v-model="resume.personalInfo.email" placeholder="Email" />
-    <input v-model="resume.personalInfo.website" placeholder="Website" />
-    <input v-model="resume.personalInfo.github" placeholder="GitHub Username" />
-    <input v-model="resume.personalInfo.linkedin" placeholder="LinkedIn Username" />
+    <h2 class="text-xl underline">Personal Information</h2>
+    <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="resume.personalInfo.name" placeholder="Full Name" />
+    <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="resume.personalInfo.phone" placeholder="Phone Number" />
+    <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="resume.personalInfo.email" placeholder="Email" />
+    <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="resume.personalInfo.website" placeholder="Website" />
+    <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="resume.personalInfo.github" placeholder="GitHub Username" />
+    <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="resume.personalInfo.linkedin" placeholder="LinkedIn Username" />
 
-    <h2>Career Profile</h2>
-    <textarea v-model="resume.careerProfile" placeholder="Career profile"></textarea>
+    <h2 class="text-xl underline">Career Profile</h2>
+    <textarea class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="resume.careerProfile" placeholder="Career profile"></textarea>
 
-    <h2>Experience</h2>
+    <h2 class="text-xl underline">Experience</h2>
     <div v-for="(exp, index) in resume.experience" :key="index">
-      <input v-model="exp.role" placeholder="Job Role" />
-      <input v-model="exp.period" placeholder="Period" />
-      <input v-model="exp.company" placeholder="Company" />
-      <input v-model="exp.location" placeholder="Location" />
-      <textarea v-model="exp.achievements" placeholder="Achievements (one per line)"></textarea>
+      <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="exp.role" placeholder="Job Role" />
+      <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="exp.period" placeholder="Period" />
+      <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="exp.company" placeholder="Company" />
+      <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="exp.location" placeholder="Location" />
+      <textarea class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="exp.achievements" placeholder="Achievements (one per line)"></textarea>
     </div>
     <button @click="addExperience">Add Experience</button>
 
-    <h2>Skills</h2>
+    <h2 class="text-xl underline">Skills</h2>
     <div v-for="(skillSet, index) in resume.skills" :key="index">
-      <input v-model="skillSet.category" placeholder="Skill Category" />
-      <input v-model="skillSet.items" placeholder="Skills (comma-separated)" />
+      <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="skillSet.category" placeholder="Skill Category" />
+      <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="skillSet.items" placeholder="Skills (comma-separated)" />
     </div>
     <button @click="addSkillSet">Add Skill Set</button>
 
-    <h2>Education</h2>
+    <h2 class="text-xl underline">Education</h2>
     <div v-for="(edu, index) in resume.education" :key="index">
-      <input v-model="edu.institution" placeholder="Institution" />
-      <input v-model="edu.period" placeholder="Period" />
-      <input v-model="edu.degree" placeholder="Degree/Specialization" />
-      <input v-model="edu.gpa" placeholder="GPA" />
-      <input v-model="edu.modules" placeholder="Relevant modules (comma-separated)" />
-      <input v-model="edu.project" placeholder="Project" />
-      <textarea v-model="edu.achievements" placeholder="Achievements (one per line)"></textarea>
+      <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="edu.institution" placeholder="Institution" />
+      <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="edu.period" placeholder="Period" />
+      <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="edu.degree" placeholder="Degree/Specialization" />
+      <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="edu.gpa" placeholder="GPA" />
+      <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="edu.modules" placeholder="Relevant modules (comma-separated)" />
+      <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="edu.project" placeholder="Project" />
+      <textarea class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="edu.achievements" placeholder="Achievements (one per line)"></textarea>
     </div>
     <button @click="addEducation">Add Education</button>
 
-    <h2>Projects</h2>
+    <h2 class="text-xl underline">Projects</h2>
     <div v-for="(project, index) in resume.projects" :key="index">
-      <input v-model="project.name" placeholder="Project Name" />
-      <input v-model="project.period" placeholder="Period" />
-      <input v-model="project.tools" placeholder="Tools Used" />
-      <textarea v-model="project.description" placeholder="Project Description (one point per line)"></textarea>
+      <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="project.name" placeholder="Project Name" />
+      <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="project.period" placeholder="Period" />
+      <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="project.tools" placeholder="Tools Used" />
+      <textarea class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="project.description" placeholder="Project Description (one point per line)"></textarea>
     </div>
     <button @click="addProject">Add Project</button>
 
-    <h2>Achievements</h2>
+    <h2 class="text-xl underline">Achievements</h2>
     <div v-for="(achievement, index) in resume.achievements" :key="index">
-      <input v-model="achievement.title" placeholder="Achievement Title" />
-      <input v-model="achievement.description" placeholder="Achievement Description" />
+      <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="achievement.title" placeholder="Achievement Title" />
+      <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="achievement.description" placeholder="Achievement Description" />
     </div>
     <button @click="addAchievement">Add Achievement</button>
 
-    <h2>Languages</h2>
-    <input v-model="resume.languages" placeholder="Languages (comma-separated)" />
+    <h2 class="text-xl underline">Languages</h2>
+    <input class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="resume.languages" placeholder="Languages (comma-separated)" />
 
-    <h2>Interests</h2>
-    <input v-model="resume.interests" placeholder="Interests (comma-separated)" />
+    <h2 class="text-xl underline">Interests</h2>
+    <input class="  block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="resume.interests" placeholder="Interests (comma-separated)" />
 
-    <button @click="generateLatex">Generate LaTeX</button>
+    <div class="mt-10 cursor-pointer text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900" @click="generateLatex">Generate LaTeX</div>
   </div>
 </template>
 
